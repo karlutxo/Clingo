f8838a5311b381f760da5d75c1df9372456d1ec3
karlutxo <karlutxo@gmail.com>
2022-06-16 13:17:35 +0100

    Initial commit

--- klingo/models.py
+++ klingo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-from django.db
+
+
 # Create your models here.
+
+class Verb(models.Model):
+    Infinitive = models.CharField(max_length=30)
+    order = models.IntegerField
+    group = models.IntegerField
+    lang = models.CharField(max_length=2, default='fr')
